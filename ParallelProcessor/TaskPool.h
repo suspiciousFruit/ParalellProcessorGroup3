0a521938438d4d620971eae21052a171073decb0
suspiciousFruit <65548731+suspiciousFruit@users.noreply.github.com>
2020-05-18 20:19:20 +0300

    Added interfaces

---
+++ ParallelProcessor/TaskPool.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include "Task.h"
+
+
+
+// Класс-интерфейс конкретного представителя делегатора задач
+class TaskPool
+{
+public:
+	virtual Task* getTask() = 0; // Получить следующую задачу
+	virtual void setCompleted(Task*) = 0; // Сообщить TaskPool о том что задача выполнена
+	// Все представители исполнитей должны по окончанию выполнения задачи вызывать этот метод
+	virtual ~TaskPool() = 0;
+};
